680f99bd01bd2bceb1366d28dd4de980529b1cfe
iHuzaifaEhsan <iamhuzaifaehsan@gmail.com>
2025-01-08 17:55:26 +0500

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useContext } from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import arrow from '../../assets/arrow_icon.png'
+import { CoinContext } from '../../context/CoinContext'
+
+const Navbar = () => {
+
+  const { setCurrency } = useContext(CoinContext);
+
+  const currencyHandler = (e) => {
+    switch (e.target.value) {
+      case "usd": {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+      case "eur": {
+        setCurrency({ name: "eur", symbol: "€" });
+        break;
+      }
+      case "inr": {
+        setCurrency({ name: "inr", symbol: "₹" });
+        break;
+      }
+      case "pkr": {
+        setCurrency({ name: "pkr", symbol: "Rs." });
+        break;
+      }
+      default: {
+        setCurrency({ name: "usd", symbol: "$" });
+        break;
+      }
+    }
+  }
+
+  return (
+    <div className='navbar'>
+      <img src={logo} alt="" className='logo' />
+      <ul>
+        <li>Home</li>
+        <li>Features</li>
+        <li>Pricing</li>
+        <li>Blog</li>
+      </ul>
+      <div className="nav-right">
+        <select onChange={currencyHandler}>
+          <option value="usd">USD</option>
+          <option value="eur">EUR</option>
+          <option value="inr">INR</option>
+          <option value="pkr">PKR</option>
+        </select>
+        <button>Sign up <img src={arrow} alt="" /></button>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
